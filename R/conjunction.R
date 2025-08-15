3dbf1bb0bedd02d10f2b91985f76c9d7270b449a
willrmull <willrmullins96@gmail.com>
2025-08-15 13:34:31 -0700

    Initial commit

---
+++ R/conjunction.R
@@ -0,0 +1,17 @@
+#' Conjunction
+#'
+#' @param name A string containing the name of a person
+#' @param date A string containing the date of birth of in the format of Day/Month/Year
+#' @param social A numeric variable containing the social security number of user
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' conjunction("Melanie", "??/??/????", 123456789)
+conjunction <- function(name, date, social) {
+  list_variable <- paste("Name:", name, '\n', "Date of Birth:", date, '\n', "Social Security Number:", social)
+  cat(list_variable[1])
+}
+conjunction("Melanie", "??/??/????", 100)
+
